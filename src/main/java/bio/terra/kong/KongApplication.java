bd8121eda4f5956cb78dfed26291561cc93af42a
Isaac Zarsky <zarsky@broadinstitute.org>
2021-05-14 13:24:22 -0400

    spring boot skeleton

---
+++ src/main/java/bio/terra/kong/KongApplication.java
@@ -0,0 +1,12 @@
+package bio.terra.kong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KongApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(KongApplication.class, args);
+  }
+}
---
+++ src/test/java/bio/terra/kong/KongApplicationTests.java
@@ -0,0 +1,11 @@
+package bio.terra.kong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KongApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
