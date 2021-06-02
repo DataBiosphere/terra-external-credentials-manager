0eb106ef6d9ecd028b0f0fa710d758a965c44fc4
Isaac Zarsky <zarsky@broadinstitute.org>
2021-06-02 11:38:16 -0400

    rename to ecm (#2)
    Bye kong, we hardly knew ye

--- src/main/java/bio/terra/externalcreds/ExternalCredsApplication.java
+++ src/main/java/bio/terra/externalcreds/ExternalCredsApplication.java
@@ -1,12 +1,12 @@
-package bio.terra.kong;
+package bio.terra.externalcreds;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication(scanBasePackages = {"bio.terra.common.logging"})
-public class KongApplication {
+public class ExternalCredsApplication {
 
   public static void main(String[] args) {
-    SpringApplication.run(KongApplication.class, args);
+    SpringApplication.run(ExternalCredsApplication.class, args);
   }
 }
