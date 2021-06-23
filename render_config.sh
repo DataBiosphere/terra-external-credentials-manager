8072542a65d50bb22ab991c8cee8f17d9ca0d738
Isaac Zarsky <zarsky@broadinstitute.org>
2021-06-23 14:56:34 -0400

    new providers config

--- render_config.sh
+++ render_config.sh
@@ -9,16 +9,7 @@ VAULT_COMMAND="vault"
 OUTPUT_LOCATION="$(dirname "$0")/service/src/main/resources/rendered/providers.yaml"
 
 if ! [ -x "$(command -v vault)" ]; then
-  VAULT_COMMAND="docker run --rm -e VAULT_TOKEN=$VAULT_TOKEN -e VAULT_ADDR=$VAULT_ADDR vault:1.7.2 $VAULT_COMMAND"
+  VAULT_COMMAND="docker run --rm -e VAULT_TOKEN=$VAULT_TOKEN -e VAULT_ADDR=$VAULT_ADDR vault:1.7.3 $VAULT_COMMAND"
 fi
 
-echo > "$OUTPUT_LOCATION"
-
-for PROVIDER in $($VAULT_COMMAND list -format=yaml "$VAULT_PATH" | sed  's/- //g')
-do
-  echo "externalcreds.providers.services.$PROVIDER:" >> "$OUTPUT_LOCATION"
-  $VAULT_COMMAND read -format=yaml -field=data "$VAULT_PATH/$PROVIDER" | while read -r YAML_LINE; do
-    echo "  $YAML_LINE" >> "$OUTPUT_LOCATION"
-  done
-  echo >> "$OUTPUT_LOCATION"
-done
+echo "externalcreds.providers.services: $($VAULT_COMMAND read -field=providers "$VAULT_PATH")" > "$OUTPUT_LOCATION"
