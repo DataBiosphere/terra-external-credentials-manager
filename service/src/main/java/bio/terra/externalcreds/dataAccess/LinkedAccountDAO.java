da045773e6788a7d411aa41889dc4f4d47775277
Kai <kfeldman@broadinstitute.org>
2021-06-24 16:17:53 -0400

    extract another helper function
    Co-Authored-By: Sarah Wessel <23193756+snwessel@users.noreply.github.com>

--- service/src/main/java/bio/terra/externalcreds/dataAccess/LinkedAccountDAO.java
+++ service/src/main/java/bio/terra/externalcreds/dataAccess/LinkedAccountDAO.java
@@ -1,6 +1,7 @@
 package bio.terra.externalcreds.dataAccess;
 
 import bio.terra.externalcreds.models.LinkedAccount;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -16,42 +17,21 @@ public class LinkedAccountDAO {
     this.jdbcTemplate = jdbcTemplate;
   }
 
+  public LinkedAccount getLinkedAccount(ResultSet rs) throws SQLException {
+    return LinkedAccount.builder()
+        .id(rs.getInt("id"))
+        .userId(rs.getString("user_id"))
+        .providerId(rs.getString("provider_id"))
+        .refreshToken(rs.getString("refresh_token"))
+        .expires(rs.getTimestamp("expires"))
+        .externalUserId(rs.getString("external_user_id"))
+        .build();
+  }
+
   public LinkedAccount getLinkedAccount(String userId, String providerId) throws SQLException {
     String query = "SELECT * FROM linked_account WHERE user_id = ? and provider_id = ?";
-    // TODO: used named parameters here
-
-    //    System.out.println(query);
-    //    PreparedStatement ps = connection.prepareStatement(query);
-    //    ps.setString(1, userId);
-    //    ps.setString(2, providerId);
-    //    ResultSet rs = ps.executeQuery();
-    //
-    //    LinkedAccount link =
-    //        LinkedAccount.builder()
-    //            .id(rs.getInt("id"))
-    //            .userId(rs.getString("user_id"))
-    //            .providerId(rs.getString("provider_id"))
-    //            .refreshToken(rs.getString("refresh_token"))
-    //            .expires(rs.getTimestamp("expires"))
-    //            .externalUserId(rs.getString("external_user_id"))
-    //            .build();
-    //    return link;
-
-    // return jdbcTemplate.queryForObject(query, LinkedAccount.class, userId, providerId);
-
-    // TODO create a RowMapper for this, so we have a reusable thing
+    // TODO: used named parameters here and create a RowMapper to pass into QueryForObject?
     return jdbcTemplate.queryForObject(
-        query,
-        (rs, rowNum) ->
-            LinkedAccount.builder()
-                .id(rs.getInt("id"))
-                .userId(rs.getString("user_id"))
-                .providerId(rs.getString("provider_id"))
-                .refreshToken(rs.getString("refresh_token"))
-                .expires(rs.getTimestamp("expires"))
-                .externalUserId(rs.getString("external_user_id"))
-                .build(),
-        userId,
-        providerId);
+        query, (rs, rowNum) -> getLinkedAccount(rs), userId, providerId);
   }
 }
