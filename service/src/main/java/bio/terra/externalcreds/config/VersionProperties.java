427a0e69405f2158db83977918a9f07f3e1b1d7b
Isaac Zarsky <zarsky@broadinstitute.org>
2021-06-25 17:35:50 -0400

    create version endpoint

---
+++ service/src/main/java/bio/terra/externalcreds/config/VersionProperties.java
@@ -0,0 +1,19 @@
+package bio.terra.externalcreds.config;
+
+import com.fasterxml.jackson.annotation.JsonIncludeProperties;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConfigurationProperties("git")
+@Getter
+@Setter
+@JsonIncludeProperties({"gitTag", "gitHash", "github", "build"})
+public class VersionProperties {
+  private String gitTag;
+  private String gitHash;
+  private String github;
+  private String build;
+}
--- service/src/main/java/bio/terra/externalcreds/controllers/PublicApiController.java
+++ service/src/main/java/bio/terra/externalcreds/controllers/PublicApiController.java
@@ -1,28 +1,34 @@
 package bio.terra.externalcreds.controllers;
 
+import bio.terra.externalcreds.config.VersionProperties;
 import bio.terra.externalcreds.generated.api.PublicApi;
 import bio.terra.externalcreds.generated.model.SystemStatus;
 import bio.terra.externalcreds.services.StatusService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class PublicApiController implements PublicApi {
 
   private final StatusService statusService;
+  private final VersionProperties versionProperties;
 
-  public PublicApiController(StatusService statusService) {
+  public PublicApiController(StatusService statusService, VersionProperties versionProperties) {
     this.statusService = statusService;
+    this.versionProperties = versionProperties;
   }
 
   @Override
-  @GetMapping("/status")
   public ResponseEntity<SystemStatus> getStatus() {
     SystemStatus currentStatus = statusService.getSystemStatus();
 
     return new ResponseEntity<>(
         currentStatus, currentStatus.isOk() ? HttpStatus.OK : HttpStatus.INTERNAL_SERVER_ERROR);
+  }
+
+  @Override
+  public ResponseEntity<VersionProperties> getVersion() {
+    return new ResponseEntity<>(versionProperties, HttpStatus.OK);
   }
 }
