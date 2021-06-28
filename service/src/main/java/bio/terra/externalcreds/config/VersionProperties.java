a0b0fda1734553212f085513f1e61807e31aa6c6
Isaac Zarsky <zarsky@broadinstitute.org>
2021-06-28 16:15:09 -0400

    consistent naming

--- service/src/main/java/bio/terra/externalcreds/config/VersionProperties.java
+++ service/src/main/java/bio/terra/externalcreds/config/VersionProperties.java
@@ -7,7 +7,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ConfigurationProperties("git")
+@ConfigurationProperties("version")
 @Getter
 @Setter
 @JsonIncludeProperties({"gitTag", "gitHash", "github", "build"})
