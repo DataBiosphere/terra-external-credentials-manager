db7dac3cae17629fdb5e8d0029fa46698c77ed0a
Sarah Wessel <swessel@broadinstitute.org>
2021-08-03 16:05:49 -0400

    Improve gradle dependencies

--- integration/src/main/java/scripts/testscripts/ServiceStatus.java
+++ integration/src/main/java/scripts/testscripts/ServiceStatus.java
@@ -7,15 +7,12 @@ import java.time.Duration;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 import scripts.utils.ClientTestUtils;
 
+@Slf4j
 public class ServiceStatus extends TestScript {
-    // TODO: find a way to get the annotation working so we don't need this
-    private static final Logger logger = LoggerFactory.getLogger(ServiceStatus.class);
     private Duration delay = Duration.ZERO;
-
 
     @Override
     public void setParameters(List<String> parameters) {
@@ -30,12 +27,12 @@ public class ServiceStatus extends TestScript {
     public void userJourney(TestUserSpecification testUser) throws Exception {
         if (delay.getSeconds() > 0) TimeUnit.SECONDS.sleep(delay.getSeconds());
 
-         logger.info("Checking service status endpoint now.");
+         log.info("Checking service status endpoint now.");
          var apiClient = ClientTestUtils.getClientWithoutAccessToken(server);
          var publicApi = new PublicApi(apiClient);
          publicApi.getStatus();
          var httpCode = publicApi.getApiClient().getStatusCode();
-         logger.info("Service status return code: {}", httpCode);
+         log.info("Service status return code: {}", httpCode);
     }
 }
 
