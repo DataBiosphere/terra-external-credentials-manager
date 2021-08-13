d97de9247b46a9a973c0fe0cf4d99449fcf764f4
Sarah Wessel <swessel@broadinstitute.org>
2021-08-13 10:46:50 -0400

    Add assertion to test

--- integration/src/main/java/scripts/testscripts/ServiceStatus.java
+++ integration/src/main/java/scripts/testscripts/ServiceStatus.java
@@ -1,5 +1,7 @@
 package scripts.testscripts;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import bio.terra.externalcreds.api.PublicApi;
 import bio.terra.testrunner.runner.TestScript;
 import bio.terra.testrunner.runner.config.TestUserSpecification;
@@ -7,6 +9,7 @@ import java.time.Duration;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
+import com.google.api.client.http.HttpStatusCodes;
 import lombok.extern.slf4j.Slf4j;
 import scripts.utils.ClientTestUtils;
 
@@ -27,11 +30,12 @@ public class ServiceStatus extends TestScript {
     public void userJourney(TestUserSpecification testUser) throws Exception {
         if (delay.getSeconds() > 0) TimeUnit.SECONDS.sleep(delay.getSeconds());
 
-         log.info("Checking service status endpoint now.");
+         log.info("Checking the service status endpoint.");
          var apiClient = ClientTestUtils.getClientWithoutAccessToken(server);
          var publicApi = new PublicApi(apiClient);
          publicApi.getStatus();
          var httpCode = publicApi.getApiClient().getStatusCode();
+         assertEquals(HttpStatusCodes.STATUS_CODE_OK, httpCode);
          log.info("Service status return code: {}", httpCode);
     }
 }
