2056584ef40c9bbc0c7acf97bb47b497af0d25d6
Sarah Wessel <swessel@broadinstitute.org>
2021-08-24 16:06:57 -0400

    Test Runner Initial Setup [POL-48] (#47)
    * Basic gradle setup for integration
    
    * First pass at creating status test
    
    * Copy GH action and config files from WSM
    
    * Get status test to build
    
    * Debugging - works locally now!
    
    * Tidy up GitHub Action
    
    * Improve gradle dependencies
    
    * Add a README and rename for clarity
    
    * Debug gradle test runner dep
    
    * QA-1542: Set up dsde-dev environment to support TestRunner in terra-external-credentials-manager repo. (#44)
    
    * A bare minimum config used by TestRunner to run tests against externalcreds-service.
    
    * Added GHA workflow and related files for running tests against GKE ecm service in dsde-dev.
    
    * Move dev directory one-level up.
    
    * Removed irrelevant code.
    
    * Clean up and debug gradle
    
    * Rename test, debug suites
    
    * Update the integration readme
    
    * Move lombok config
    
    * Rename server config files
    
    * Simplify write-configs.sh
    
    * Major script improvements
    
    * Remove lots of unnecessary dev env stuff
    
    * Combine scripts
    
    * Fix up github actions
    
    * Simplify github action, delete nightly one
    
    * Improve and simplify the GH action
    
    * Get secretes from Github instead of vault
    
    * Debug pr-integration action
    
    * Update rendered secrets location
    
    * Final tidying of changes
    
    * Fix a comment
    
    * Simplify GHA
    
    * Debug action
    
    * Update background server version
    
    * Debug action
    
    * Update READMEs
    
    * Add assertion to test
    
    * Fix background server action
    
    * More debugging
    
    * debugging
    
    * debug
    
    * debug2
    
    * dubug3
    
    * Revert action changes
    
    * QA-1554: Test Runner upgrade to support optional URI properties and add a new property for policy manager URI. (#53)
    
    * Removed irrelevant code.
    
    * Test upgraded TestRunner with policyManagerUri.
    
    * Revert changes used for testing.
    
    * Update README.md
    
    * Remove unneeded params, apply spotless
    
    * Avoid repetition in GHA
    
    * debug
    
    * debugging
    
    * clarifying intent
    
    * Revert "clarifying intent"
    
    This reverts commit a4b8d1ca49aed55e34d150174a935fdd4ae72a20.
    
    * experiment
    
    Co-Authored-By: Isaac Zarsky <22642695+zarsky-broad@users.noreply.github.com>
    
    * Revert "experiment"
    
    This reverts commit e56c740bfe3a785d76e11af173d73dda047dd906.
    
    * bumping version
    
    Co-authored-by: ichengchang <50877414+ichengchang@users.noreply.github.com>
    Co-authored-by: Isaac Zarsky <22642695+zarsky-broad@users.noreply.github.com>

---
+++ integration/README.md
@@ -0,0 +1,26 @@
+# `integration` Project
+
+This Gradle project contains Test Runner integration tests for the External Credentials Manager. 
+    
+
+### Adding New Tests
+
+1. Create a new script in the [test scripts directory](src/main/java/scripts/testscripts). 
+2. Add a reference to the test script in our FullIntegration test suite [here](src/main/resources/suites/FullIntegration.json).
+3. Debug the test locally using the instructions below. 
+
+
+### Running Integration Tests
+
+The test runner task `runTest` can be used to launch individual tests or entire test suites. 
+
+To run the tests locally:
+
+1. Follow the initial setup instructions described in the [DEVELOPMENT.md](../DEVELOPMENT.md). 
+2. Run the `ExternalCredsApplication` (in IntelliJ).
+3. Then, run the integration tests in IntelliJ using the "Run local Integration" run configuration, or on the command line using the `runTest` command:
+   - To run a test suite (ex. Full Integration suite):
+     `./gradlew runTest --args="suites/dev/FullIntegration.json /tmp/TR`
+   - To run a single test (ex. Service Status test)
+     `./gradlew runTest --args="configs/integration/ServiceStatus.json /tmp/TR"`
+   - To debug, add `--stacktrace`.
---
+++ integration/src/main/java/scripts/utils/ClientTestUtils.java
@@ -0,0 +1,42 @@
+package scripts.utils;
+
+import bio.terra.externalcreds.client.ApiClient;
+import bio.terra.testrunner.runner.config.ServerSpecification;
+import com.google.auth.oauth2.AccessToken;
+import com.google.common.base.Strings;
+import java.io.IOException;
+import javax.annotation.Nullable;
+
+public class ClientTestUtils {
+
+  private ClientTestUtils() {}
+
+  /**
+   * Build the ECM API client object for the server specifications. No access token is needed for
+   * this API client.
+   *
+   * @param server the server we are testing against
+   * @return the API client object for this user
+   */
+  public static ApiClient getClientWithoutAccessToken(ServerSpecification server)
+      throws IOException {
+    return buildClient(null, server);
+  }
+
+  private static ApiClient buildClient(
+      @Nullable AccessToken accessToken, ServerSpecification server) throws IOException {
+    if (Strings.isNullOrEmpty(server.policyManagerUri)) {
+      throw new IllegalArgumentException("Service URI cannot be empty");
+    }
+
+    // build the client object
+    ApiClient apiClient = new ApiClient();
+    apiClient.setBasePath(server.policyManagerUri);
+
+    if (accessToken != null) {
+      apiClient.setAccessToken(accessToken.getTokenValue());
+    }
+
+    return apiClient;
+  }
+}
