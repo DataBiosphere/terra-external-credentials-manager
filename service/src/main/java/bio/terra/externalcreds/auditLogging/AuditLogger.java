ecb1a176d00e732843809de73cacadf139de66b3
Douglas Voet <dvoet@users.noreply.github.com>
2021-09-10 09:45:36 -0400

    comment

--- service/src/main/java/bio/terra/externalcreds/auditLogging/AuditLogger.java
+++ service/src/main/java/bio/terra/externalcreds/auditLogging/AuditLogger.java
@@ -4,6 +4,10 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
+/**
+ * AuditLogger as a separate component to centralize audit logging functions and ensure that all
+ * audit events use the same logger.
+ */
 @Component
 @Slf4j
 public class AuditLogger {
@@ -14,6 +18,8 @@ public class AuditLogger {
   }
 
   public void logEvent(AuditLogEvent event) {
+    // mapper.valueToTree(event) converts event to a JsonNode which is handled specially by
+    // bio.terra.common.logging.GoogleJsonLayout
     log.info(event.getAuditLogEventType().toString(), mapper.valueToTree(event));
   }
 }
