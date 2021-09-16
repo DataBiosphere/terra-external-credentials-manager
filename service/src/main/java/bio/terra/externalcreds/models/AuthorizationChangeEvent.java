e1ef31b7a42bc9b1b91e4e597ade115f56d8572e
dvoet <dvoet@users.noreply.github.com>
2021-09-16 13:37:29 -0400

    POL-57 pubsub mechanics (#66)

---
+++ service/src/main/java/bio/terra/externalcreds/models/AuthorizationChangeEvent.java
@@ -0,0 +1,14 @@
+package bio.terra.externalcreds.models;
+
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import org.immutables.value.Value;
+
+@Value.Immutable
+@JsonSerialize(as = ImmutableAuthorizationChangeEvent.class)
+public interface AuthorizationChangeEvent extends WithAuthorizationChangeEvent {
+  String getUserId();
+
+  String getProviderId();
+
+  class Builder extends ImmutableAuthorizationChangeEvent.Builder {}
+}
