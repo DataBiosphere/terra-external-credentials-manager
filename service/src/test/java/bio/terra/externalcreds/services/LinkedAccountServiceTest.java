668ca20a0438fa3bfbe7e85c22c66577502cda2f
Douglas Voet <dvoet@users.noreply.github.com>
2021-09-22 09:51:43 -0400

    disabled test

--- service/src/test/java/bio/terra/externalcreds/services/LinkedAccountServiceTest.java
+++ service/src/test/java/bio/terra/externalcreds/services/LinkedAccountServiceTest.java
@@ -9,6 +9,7 @@ import bio.terra.externalcreds.TestUtils;
 import bio.terra.externalcreds.dataAccess.GA4GHPassportDAO;
 import bio.terra.externalcreds.dataAccess.GA4GHVisaDAO;
 import bio.terra.externalcreds.dataAccess.LinkedAccountDAO;
+import bio.terra.externalcreds.models.AuthorizationChangeEvent;
 import bio.terra.externalcreds.models.GA4GHPassport;
 import bio.terra.externalcreds.models.GA4GHVisa;
 import bio.terra.externalcreds.models.LinkedAccount;
@@ -18,8 +19,10 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.TestComponent;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -69,7 +72,9 @@ public class LinkedAccountServiceTest extends BaseTest {
               TestUtils.createRandomVisa(),
               TestUtils.createRandomVisa(),
               TestUtils.createRandomVisa());
-      var savedLinkedAccount1 = saveAndValidateLinkedAccount(linkedAccount, passport, visas);
+      var savedLinkedAccount1 =
+          saveAndValidateLinkedAccount(
+              linkedAccount, passport, visas, linkedAccountService, passportDAO, visaDAO);
 
       // save again with new passport and visas to test overwrite
       var passport2 = TestUtils.createRandomPassport();
@@ -78,7 +83,9 @@ public class LinkedAccountServiceTest extends BaseTest {
               TestUtils.createRandomVisa(),
               TestUtils.createRandomVisa(),
               TestUtils.createRandomVisa());
-      var savedLinkedAccount2 = saveAndValidateLinkedAccount(linkedAccount, passport2, visas2);
+      var savedLinkedAccount2 =
+          saveAndValidateLinkedAccount(
+              linkedAccount, passport2, visas2, linkedAccountService, passportDAO, visaDAO);
 
       // saved linked accounts should the same
       assertEquals(savedLinkedAccount1, savedLinkedAccount2);
@@ -88,51 +95,20 @@ public class LinkedAccountServiceTest extends BaseTest {
     void testSaveLinkedAccountWithPassportNoVisas() {
       var linkedAccount = TestUtils.createRandomLinkedAccount();
       var passport = TestUtils.createRandomPassport();
-      saveAndValidateLinkedAccount(linkedAccount, passport, Collections.emptyList());
+      saveAndValidateLinkedAccount(
+          linkedAccount,
+          passport,
+          Collections.emptyList(),
+          linkedAccountService,
+          passportDAO,
+          visaDAO);
     }
 
     @Test
     void testSaveLinkedAccountWithoutPassport() {
       var linkedAccount = TestUtils.createRandomLinkedAccount();
-      saveAndValidateLinkedAccount(linkedAccount, null, Collections.emptyList());
-    }
-
-
-    private LinkedAccount saveAndValidateLinkedAccount(
-        LinkedAccount linkedAccount, GA4GHPassport passport, List<GA4GHVisa> visas) {
-      var saved =
-          linkedAccountService.upsertLinkedAccountWithPassportAndVisas(
-              new LinkedAccountWithPassportAndVisas.Builder()
-                  .linkedAccount(linkedAccount)
-                  .passport(Optional.ofNullable(passport))
-                  .visas(visas)
-                  .build());
-
-      assertEquals(linkedAccount, saved.getLinkedAccount().withId(Optional.empty()));
-      assertTrue(saved.getLinkedAccount().getId().isPresent());
-
-      var savedPassport =
-          passportDAO.getPassport(
-              saved.getLinkedAccount().getUserId(), saved.getLinkedAccount().getProviderId());
-      if (passport == null) {
-        assertEmpty(savedPassport);
-      } else {
-        assertPresent(savedPassport);
-        assertPresent(savedPassport.get().getId());
-        assertEquals(
-            passport,
-            savedPassport.get().withId(Optional.empty()).withLinkedAccountId(Optional.empty()));
-        var savedVisas =
-            visaDAO.listVisas(
-                saved.getLinkedAccount().getUserId(), saved.getLinkedAccount().getProviderId());
-        assertEquals(
-            visas,
-            savedVisas.stream()
-                .map(v -> v.withId(Optional.empty()).withPassportId(Optional.empty()))
-                .collect(Collectors.toList()));
-      }
-
-      return saved.getLinkedAccount();
+      saveAndValidateLinkedAccount(
+          linkedAccount, null, Collections.emptyList(), linkedAccountService, passportDAO, visaDAO);
     }
   }
 
@@ -140,6 +116,8 @@ public class LinkedAccountServiceTest extends BaseTest {
   @TestComponent
   class DeleteLinkedAccount {
     @Autowired private LinkedAccountService linkedAccountService;
+    @Autowired private GA4GHPassportDAO passportDAO;
+    @Autowired private GA4GHVisaDAO visaDAO;
 
     @Test
     void testDeleteNonExistingLinkedAccount() {
@@ -148,6 +126,92 @@ public class LinkedAccountServiceTest extends BaseTest {
           linkedAccountService.deleteLinkedAccount(
               linkedAccount.getUserId(), linkedAccount.getProviderId()));
     }
+
+    @Test
+    void testDeleteExistingLinkedAccount() {
+      var linkedAccount = TestUtils.createRandomLinkedAccount();
+      var passport = TestUtils.createRandomPassport();
+      var visas =
+          List.of(
+              TestUtils.createRandomVisa(),
+              TestUtils.createRandomVisa(),
+              TestUtils.createRandomVisa());
+      var savedLinkedAccount1 =
+          saveAndValidateLinkedAccount(
+              linkedAccount, passport, visas, linkedAccountService, passportDAO, visaDAO);
+
+      assertTrue(
+          linkedAccountService.deleteLinkedAccount(
+              linkedAccount.getUserId(), linkedAccount.getProviderId()));
+
+      assertEmpty(linkedAccountService.getLinkedAccount(savedLinkedAccount1.getId().get()));
+    }
+
+    @Test
+    @Disabled
+    void testDeleteLinkedAccountEmitsEvent() {
+      var linkedAccount = TestUtils.createRandomLinkedAccount();
+      var passport = TestUtils.createRandomPassport();
+      var visas =
+          List.of(
+              TestUtils.createRandomVisa(),
+              TestUtils.createRandomVisa(),
+              TestUtils.createRandomVisa());
+      saveAndValidateLinkedAccount(
+          linkedAccount, passport, visas, linkedAccountService, passportDAO, visaDAO);
+
+      var spy = Mockito.spy(linkedAccountService);
+      spy.deleteLinkedAccount(linkedAccount.getUserId(), linkedAccount.getProviderId());
+
+      Mockito.verify(spy)
+          .publishAuthorizationChangeEvent(
+              new AuthorizationChangeEvent.Builder()
+                  .userId(linkedAccount.getUserId())
+                  .providerId(linkedAccount.getProviderId())
+                  .build());
+    }
+  }
+
+  private LinkedAccount saveAndValidateLinkedAccount(
+      LinkedAccount linkedAccount,
+      GA4GHPassport passport,
+      List<GA4GHVisa> visas,
+      LinkedAccountService linkedAccountService,
+      GA4GHPassportDAO passportDAO,
+      GA4GHVisaDAO visaDAO) {
+    var saved =
+        linkedAccountService.upsertLinkedAccountWithPassportAndVisas(
+            new LinkedAccountWithPassportAndVisas.Builder()
+                .linkedAccount(linkedAccount)
+                .passport(Optional.ofNullable(passport))
+                .visas(visas)
+                .build());
+
+    assertEquals(linkedAccount, saved.getLinkedAccount().withId(Optional.empty()));
+    assertTrue(saved.getLinkedAccount().getId().isPresent());
+
+    var savedPassport =
+        passportDAO.getPassport(
+            saved.getLinkedAccount().getUserId(), saved.getLinkedAccount().getProviderId());
+    if (passport == null) {
+      assertEmpty(savedPassport);
+    } else {
+      assertPresent(savedPassport);
+      assertPresent(savedPassport.get().getId());
+      assertEquals(
+          passport,
+          savedPassport.get().withId(Optional.empty()).withLinkedAccountId(Optional.empty()));
+      var savedVisas =
+          visaDAO.listVisas(
+              saved.getLinkedAccount().getUserId(), saved.getLinkedAccount().getProviderId());
+      assertEquals(
+          visas,
+          savedVisas.stream()
+              .map(v -> v.withId(Optional.empty()).withPassportId(Optional.empty()))
+              .collect(Collectors.toList()));
+    }
+
+    return saved.getLinkedAccount();
   }
 
   // save new linked account emits event - 2 visa types
