93686de6eb2ba1575900720aaab0f064b3044837
Sarah Wessel <swessel@broadinstitute.org>
2022-01-20 17:07:34 -0500

    Proxy auth

--- integration/src/main/java/scripts/utils/ClientTestUtils.java
+++ integration/src/main/java/scripts/utils/ClientTestUtils.java
@@ -1,14 +1,23 @@
 package scripts.utils;
 
 import bio.terra.externalcreds.client.ApiClient;
+import bio.terra.testrunner.common.utils.AuthenticationUtils;
 import bio.terra.testrunner.runner.config.ServerSpecification;
+import bio.terra.testrunner.runner.config.TestUserSpecification;
 import com.google.auth.oauth2.AccessToken;
+import com.google.auth.oauth2.GoogleCredentials;
 import com.google.common.base.Strings;
+import java.io.IOException;
+import java.util.List;
 import javax.annotation.Nullable;
 
 public class ClientTestUtils {
 
   private ClientTestUtils() {}
+
+  // Required scopes for client tests include the usual login scopes and GCP scope.
+  public static final List<String> TEST_USER_SCOPES =
+      List.of("openid", "email", "profile", "https://www.googleapis.com/auth/cloud-platform");
 
   /**
    * Build the ECM API client object for the server specifications. No access token is needed for
@@ -19,6 +28,28 @@ public class ClientTestUtils {
    */
   public static ApiClient getClientWithoutAccessToken(ServerSpecification server) {
     return buildClient(null, server);
+  }
+
+  /**
+   * Build the ECM API client object for the given test user and server specifications. The test
+   * user's token is always refreshed
+   *
+   * @param testUser the test user whose credentials are supplied to the API client object
+   * @param server the server we are testing against
+   * @return the API client object for this user
+   */
+  public static ApiClient getClientForTestUser(
+      TestUserSpecification testUser, ServerSpecification server) throws IOException {
+    // if no test user is specified, then return a client object without an access token set
+    AccessToken accessToken = null;
+
+    if (testUser != null) {
+      GoogleCredentials userCredential =
+          AuthenticationUtils.getDelegatedUserCredential(testUser, TEST_USER_SCOPES);
+      accessToken = AuthenticationUtils.getAccessToken(userCredential);
+    }
+
+    return buildClient(accessToken, server);
   }
 
   private static ApiClient buildClient(
