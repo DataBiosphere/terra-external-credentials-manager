3c7b09131ff0cb173eeba1808b8b5e3dfac187f6
Isaac Zarsky <zarsky@broadinstitute.org>
2022-03-18 14:49:35 -0400

    Create resttemplate client to publish [POL-204] (#108)

--- integration/src/main/java/scripts/testscripts/ListProviders.java
+++ integration/src/main/java/scripts/testscripts/ListProviders.java
@@ -6,8 +6,8 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 import bio.terra.externalcreds.api.OidcApi;
 import bio.terra.testrunner.runner.TestScript;
 import bio.terra.testrunner.runner.config.TestUserSpecification;
-import com.google.api.client.http.HttpStatusCodes;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import scripts.utils.ClientTestUtils;
 
 @Slf4j
@@ -19,14 +19,14 @@ public class ListProviders extends TestScript {
     var apiClient = ClientTestUtils.getClientWithTestUserAuth(testUser, server);
     var oidcApi = new OidcApi(apiClient);
 
-    var providers = oidcApi.listProviders();
+    var providersResponse = oidcApi.listProvidersWithHttpInfo();
 
     // check the response code
-    var httpCode = oidcApi.getApiClient().getStatusCode();
-    assertEquals(HttpStatusCodes.STATUS_CODE_OK, httpCode);
+    var httpCode = providersResponse.getStatusCode();
+    assertEquals(HttpStatus.OK, httpCode);
     log.info("List providers return code: {}", httpCode);
 
     // check the response body
-    assertNotNull(providers);
+    assertNotNull(providersResponse.getBody());
   }
 }
