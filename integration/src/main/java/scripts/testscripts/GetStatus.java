b58ce7cbd46daf984818e4c8ebf703c24a777785
Isaac Zarsky <zarsky@broadinstitute.org>
2022-03-18 14:22:32 -0400

    Create resttemplate client

--- integration/src/main/java/scripts/testscripts/GetStatus.java
+++ integration/src/main/java/scripts/testscripts/GetStatus.java
@@ -5,21 +5,21 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import bio.terra.externalcreds.api.PublicApi;
 import bio.terra.testrunner.runner.TestScript;
 import bio.terra.testrunner.runner.config.TestUserSpecification;
-import com.google.api.client.http.HttpStatusCodes;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import scripts.utils.ClientTestUtils;
 
 @Slf4j
 public class GetStatus extends TestScript {
 
   @Override
-  public void userJourney(TestUserSpecification testUser) throws Exception {
+  public void userJourney(TestUserSpecification testUser) {
     log.info("Checking the service status endpoint.");
     var apiClient = ClientTestUtils.getClientWithoutAuth(server);
     var publicApi = new PublicApi(apiClient);
-    publicApi.getStatus();
-    var httpCode = publicApi.getApiClient().getStatusCode();
-    assertEquals(HttpStatusCodes.STATUS_CODE_OK, httpCode);
+
+    var httpCode = publicApi.getStatusWithHttpInfo().getStatusCode();
+    assertEquals(HttpStatus.OK, httpCode);
     log.info("Service status return code: {}", httpCode);
   }
 }
