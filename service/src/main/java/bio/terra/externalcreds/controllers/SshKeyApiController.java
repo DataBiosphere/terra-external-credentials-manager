50986f911d1ecc5bd5a8b15fa748985241441a87
yuhuyoyo <yuhuyoyo@google.com>
2022-04-07 14:21:55 -0400

    remove l4j annotation

--- service/src/main/java/bio/terra/externalcreds/controllers/SshKeyApiController.java
+++ service/src/main/java/bio/terra/externalcreds/controllers/SshKeyApiController.java
@@ -11,12 +11,10 @@ import bio.terra.externalcreds.services.SshKeyPairService;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import javax.servlet.http.HttpServletRequest;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 
 @Controller
-@Slf4j
 public record SshKeyApiController(
     HttpServletRequest request, SamService samService, SshKeyPairService sshKeyPairService,
     ObjectMapper objectMapper)
