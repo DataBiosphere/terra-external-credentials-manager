3215fceaa6ccb4026bf33bf4d4e987599da0879c
tlangs <tlangs@broadinstitute.org>
2022-06-24 12:03:35 -0400

    Make ECM decode and validate passports in two steps

--- service/src/test/java/bio/terra/externalcreds/services/JwtUtilsTest.java
+++ service/src/test/java/bio/terra/externalcreds/services/JwtUtilsTest.java
@@ -118,8 +118,10 @@ public class JwtUtilsTest extends BaseTest {
       var issuer = "https://stsstg.nih.gov";
       when(externalCredsConfigMock.getAllowedJwtIssuers()).thenReturn(List.of(new URI(issuer)));
 
+      // These are explicitly different from the RS256 algo
+      // used to create the signed test JWT
       when(externalCredsConfigMock.getAllowedJwtAlgorithms())
-          .thenReturn(List.of("testAlg", "testAlg2"));
+          .thenReturn(List.of("EdDSA", "ES256K"));
 
       var visaClaimSet =
           new JWTClaimsSet.Builder()
