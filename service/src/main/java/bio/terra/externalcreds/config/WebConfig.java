13c943c14ad42d5a1b3bc8e31bce01391c6144b1
tlangs <tlangs@broadinstitute.org>
2024-02-13 13:20:44 -0500

    ID-1051 Add Formatter to Spring API

--- service/src/main/java/bio/terra/externalcreds/config/WebConfig.java
+++ service/src/main/java/bio/terra/externalcreds/config/WebConfig.java
@@ -2,6 +2,8 @@ package bio.terra.externalcreds.config;
 
 import bio.terra.externalcreds.logging.LoggerInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.convert.ApplicationConversionService;
+import org.springframework.format.FormatterRegistry;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -13,5 +15,10 @@ public class WebConfig implements WebMvcConfigurer {
   @Override
   public void addInterceptors(InterceptorRegistry registry) {
     registry.addInterceptor(loggerInterceptor);
+  }
+
+  @Override
+  public void addFormatters(FormatterRegistry registry) {
+    ApplicationConversionService.configure(registry);
   }
 }
