b464e0eae9e21a44c3f334679e26a577a87f9a18
Saman Ehsan <sehsan@broadinstitute.org>
2024-02-20 12:19:27 -0500

    Add /api/oauth/v1/{provider}/oauthcode endpoint
    And deprecate /api/oidc/v1/{provider}/oauthcode

--- service/src/main/java/bio/terra/externalcreds/controllers/OauthApiController.java
+++ service/src/main/java/bio/terra/externalcreds/controllers/OauthApiController.java
@@ -1,6 +1,11 @@
 package bio.terra.externalcreds.controllers;
 
+import bio.terra.common.exception.BadRequestException;
+import bio.terra.externalcreds.auditLogging.AuditLogEvent;
+import bio.terra.externalcreds.auditLogging.AuditLogEventType;
+import bio.terra.externalcreds.auditLogging.AuditLogger;
 import bio.terra.externalcreds.generated.api.OauthApi;
+import bio.terra.externalcreds.generated.model.LinkInfo;
 import bio.terra.externalcreds.generated.model.Provider;
 import bio.terra.externalcreds.services.*;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -10,9 +15,11 @@ import org.springframework.stereotype.Controller;
 
 @Controller
 public record OauthApiController(
+    AuditLogger auditLogger,
     HttpServletRequest request,
     ObjectMapper mapper,
     ProviderService providerService,
+    PassportProviderService passportProviderService,
     ExternalCredsSamUserFactory samUserFactory)
     implements OauthApi {
 
@@ -25,5 +32,38 @@ public record OauthApiController(
             samUser.getSubjectId(), providerName.toString(), redirectUri);
 
     return ResponseEntity.of(authorizationUrl);
+  }
+
+  @Override
+  public ResponseEntity<LinkInfo> createLink(
+      Provider providerName, String state, String oauthcode) {
+    var samUser = samUserFactory.from(request);
+
+    var auditLogEventBuilder =
+        new AuditLogEvent.Builder()
+            .providerName(providerName.toString())
+            .userId(samUser.getSubjectId())
+            .clientIP(request.getRemoteAddr());
+
+    try {
+      if (providerName.equals(Provider.RAS)) {
+        var linkedAccountWithPassportAndVisas =
+            passportProviderService.createLink(
+                providerName.toString(),
+                samUser.getSubjectId(),
+                oauthcode,
+                state,
+                auditLogEventBuilder);
+        return ResponseEntity.of(
+            linkedAccountWithPassportAndVisas.map(
+                x -> OpenApiConverters.Output.convert(x.getLinkedAccount())));
+      } else {
+        throw new BadRequestException("Invalid providerName");
+      }
+    } catch (Exception e) {
+      auditLogger.logEvent(
+          auditLogEventBuilder.auditLogEventType(AuditLogEventType.LinkCreationFailed).build());
+      throw e;
+    }
   }
 }
