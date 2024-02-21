2569f91f58c266e0f1a12810a70bc701454fc149
Saman Ehsan <sehsan@broadinstitute.org>
2024-02-21 11:35:16 -0500

    [ID-1050] Add Github account linking (#171)
    * Add /api/oauth/v1/{provider}/oauthcode endpoint
    
    And deprecate /api/oidc/v1/{provider}/oauthcode
    
    * Add github support to POST oauthcode endpoint
    
    * Style/format fixes
    
    * Fix unit test
    
    * Update token provider tests

--- service/src/main/java/bio/terra/externalcreds/controllers/OauthApiController.java
+++ service/src/main/java/bio/terra/externalcreds/controllers/OauthApiController.java
@@ -1,18 +1,27 @@
 package bio.terra.externalcreds.controllers;
 
+import bio.terra.externalcreds.auditLogging.AuditLogEvent;
+import bio.terra.externalcreds.auditLogging.AuditLogEventType;
+import bio.terra.externalcreds.auditLogging.AuditLogger;
+import bio.terra.externalcreds.controllers.OpenApiConverters.Output;
 import bio.terra.externalcreds.generated.api.OauthApi;
+import bio.terra.externalcreds.generated.model.LinkInfo;
 import bio.terra.externalcreds.generated.model.Provider;
 import bio.terra.externalcreds.services.*;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.http.HttpServletRequest;
+import java.util.Optional;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public record OauthApiController(
+    AuditLogger auditLogger,
     HttpServletRequest request,
     ObjectMapper mapper,
     ProviderService providerService,
+    PassportProviderService passportProviderService,
+    TokenProviderService tokenProviderService,
     ExternalCredsSamUserFactory samUserFactory)
     implements OauthApi {
 
@@ -25,5 +34,50 @@ public record OauthApiController(
             samUser.getSubjectId(), providerName.toString(), redirectUri);
 
     return ResponseEntity.of(authorizationUrl);
+  }
+
+  @Override
+  public ResponseEntity<LinkInfo> createLink(
+      Provider providerName, String state, String oauthcode) {
+    var samUser = samUserFactory.from(request);
+
+    var auditLogEventBuilder =
+        new AuditLogEvent.Builder()
+            .providerName(providerName.toString())
+            .userId(samUser.getSubjectId())
+            .clientIP(request.getRemoteAddr());
+
+    Optional<LinkInfo> linkInfo = Optional.empty();
+    try {
+      switch (providerName) {
+        case RAS -> {
+          var linkedAccountWithPassportAndVisas =
+              passportProviderService.createLink(
+                  providerName.toString(),
+                  samUser.getSubjectId(),
+                  oauthcode,
+                  state,
+                  auditLogEventBuilder);
+          linkInfo =
+              linkedAccountWithPassportAndVisas.map(
+                  x -> OpenApiConverters.Output.convert(x.getLinkedAccount()));
+        }
+        case GITHUB -> {
+          var linkedAccount =
+              tokenProviderService.createLink(
+                  providerName.toString(),
+                  samUser.getSubjectId(),
+                  oauthcode,
+                  state,
+                  auditLogEventBuilder);
+          linkInfo = linkedAccount.map(Output::convert);
+        }
+      }
+      return ResponseEntity.of(linkInfo);
+    } catch (Exception e) {
+      auditLogger.logEvent(
+          auditLogEventBuilder.auditLogEventType(AuditLogEventType.LinkCreationFailed).build());
+      throw e;
+    }
   }
 }
