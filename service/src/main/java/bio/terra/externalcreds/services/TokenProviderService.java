6163856b01e8ded2495a634eed12c8c98f918ea2
Kai <kfeldman@broadinstitute.org>
2024-02-23 12:17:08 -0500

    WIP add github refresh access token

--- service/src/main/java/bio/terra/externalcreds/services/TokenProviderService.java
+++ service/src/main/java/bio/terra/externalcreds/services/TokenProviderService.java
@@ -1,6 +1,8 @@
 package bio.terra.externalcreds.services;
 
 import bio.terra.common.exception.BadRequestException;
+import bio.terra.common.exception.NotFoundException;
+import bio.terra.externalcreds.ExternalCredsException;
 import bio.terra.externalcreds.auditLogging.AuditLogEvent;
 import bio.terra.externalcreds.auditLogging.AuditLogEventType;
 import bio.terra.externalcreds.auditLogging.AuditLogger;
@@ -11,6 +13,7 @@ import java.util.HashSet;
 import java.util.Optional;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.oauth2.core.OAuth2AuthorizationException;
+import org.springframework.security.oauth2.core.OAuth2RefreshToken;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -78,5 +81,48 @@ public class TokenProviderService extends ProviderService {
                     .map(x -> AuditLogEventType.LinkCreated)
                     .orElse(AuditLogEventType.LinkCreationFailed))
             .build());
+  }
+
+  //  Make a POST request to this URL, along with the following query parameters:
+  // https://github.com/login/oauth/access_token
+  //
+  //  Query parameter	Type	Description
+  //  client_id	string	Required. The client ID for your GitHub App. The client ID is different from
+  // the app ID. You can find the client ID on the settings page for your app.
+  //  client_secret	string	Required. The client secret for your GitHub App. You can generate a
+  // client secret on the settings page for your app.
+  //  grant_type	string	Required. The value must be "refresh_token".
+  //  refresh_token	string	Required. The refresh token that you received when you generated a user
+  // access token.
+
+  // test gets correct params for request?
+// some stuff copied from passportproviderservice.getrefreshedpassportsandvisas
+  public void getGithubAccessToken(String userId, String providerName) {
+    // get linked account
+    var linkedAccount =
+        linkedAccountService
+            .getLinkedAccount(userId, providerName)
+            .orElseThrow(() -> new NotFoundException(
+                String.format("No linked account found for UserId %s with Provider %s", userId, providerName)));
+
+    //get client registration from provider client cache
+    var clientRegistration =
+        providerClientCache
+            .getProviderClient(linkedAccount.getProviderName())
+            .orElseThrow(
+                () ->
+                    new ExternalCredsException(
+                        String.format(
+                            "Unable to find configs for the provider: %s",
+                            linkedAccount.getProviderName())));
+
+    // TODO: might need to build more pieces of data into  this request
+    var accessTokenResponse =
+        oAuth2Service.authorizeWithRefreshToken(
+            clientRegistration, new OAuth2RefreshToken(linkedAccount.getRefreshToken(), null));
+
+    // make a POST to https://github.com/login/oauth/access_token
+    // with query params:
+    //    client_id: look at revokeAccessToken, if we take in a providerProperties, then we can
   }
 }
--- service/src/test/java/bio/terra/externalcreds/services/TokenProviderServiceTest.java
+++ service/src/test/java/bio/terra/externalcreds/services/TokenProviderServiceTest.java
@@ -55,4 +55,27 @@ public class TokenProviderServiceTest extends BaseTest {
                 .clientIP(clientIP)
                 .build());
   }
+
+  // test creation of post request to github
+  // test access token request is logged on success and failure
+
+// can't really test github itself
+// test selects correct refresh token? already covered
+// test failure if no refresh token? already covered
+// test failure if no linked account found? no that's already covered
+
+  @Test
+  void testCreateGithubAccessTokenRequest() {
+    tokenProviderService.getGithubAccessToken(providerName, userId);
+
+  }
+
+  @Test
+  void testLogAccessTokenRequest() {
+    tokenProviderService.getGithubAccessToken(providerName, userId);
+
+  }
+
+
 }
+
