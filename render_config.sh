3259afe8d7180501ec0bd5e3c34f0bd3610497cd
Saman Ehsan <sehsan@broadinstitute.org>
2024-03-13 09:59:09 -0400

    [ID-1118] Add provider configs to application.yml (#177)
    * Add provider configs to app.yml
    
    Keep provider client ids and secrets
    in vault, but move all other config
    values into the application.yml so
    that it is easier to view and change.
    
    * Remove unnecessary comma
    
    * Only add RAS for non-prod envs
    
    * Fix overriding redirect uris
    
    * Fix allow redirect uris
    
    * Change providers Map to EnumMap

--- render_config.sh
+++ render_config.sh
@@ -8,13 +8,20 @@ COMMON_VAULT_PATH="secret/dsde/terra/kernel/$ENV/common"
 VAULT_COMMAND="vault read"
 
 SERVICE_OUTPUT_LOCATION="$(dirname "$0")/service/src/main/resources/rendered"
+SECRET_ENV_VARS_LOCATION="${SERVICE_OUTPUT_LOCATION}/secrets.env"
 INTEGRATION_OUTPUT_LOCATION="$(dirname "$0")/integration/src/main/resources/rendered"
 
 if ! [ -x "$(command -v vault)" ]; then
   VAULT_COMMAND="docker run --rm -e VAULT_TOKEN=$VAULT_TOKEN -e VAULT_ADDR=$VAULT_ADDR vault:1.7.3 $VAULT_COMMAND"
 fi
 
-$VAULT_COMMAND -field=providers "$ECM_VAULT_PATH/providers" >"$SERVICE_OUTPUT_LOCATION/providers.yaml"
+{
+  echo export RAS_CLIENT_ID="$($VAULT_COMMAND -field=ras_client_id "$ECM_VAULT_PATH/providers")"
+  echo export RAS_CLIENT_SECRET="$($VAULT_COMMAND -field=ras_client_secret "$ECM_VAULT_PATH/providers")"
+  echo export GITHUB_CLIENT_ID="$($VAULT_COMMAND -field=github_client_id "$ECM_VAULT_PATH/providers")"
+  echo export GITHUB_CLIENT_SECRET="$($VAULT_COMMAND -field=github_client_secret "$ECM_VAULT_PATH/providers")"
+} >> "${SECRET_ENV_VARS_LOCATION}"
+
 $VAULT_COMMAND -field=swagger-client-id "$ECM_VAULT_PATH/swagger-client-id" >"$SERVICE_OUTPUT_LOCATION/swagger-client-id"
 
 $VAULT_COMMAND -field=data -format=json "secret/dsde/firecloud/$ENV/common/firecloud-account.json" >"$INTEGRATION_OUTPUT_LOCATION/user-delegated-sa.json"
