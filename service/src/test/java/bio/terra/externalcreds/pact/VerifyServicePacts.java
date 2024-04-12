0dc73d8ea258faa4f847309f903611ae5f107a19
mspector <michael.l.spector@gmail.com>
2024-04-12 06:26:43 -0400

    VerifyServicePacts (#189)
    * VerifyServicePacts
    
    * update to include AccessTokenCacheDAO
    
    * remove unnecessary mocks
    
    * filter pact verification test during unit testing, create new task
    
    * remove unnecessary prints
    
    * return userEmail and bearerToken as provider states

---
+++ service/src/test/java/bio/terra/externalcreds/pact/VerifyServicePacts.java
@@ -0,0 +1,127 @@
+package bio.terra.externalcreds.pact;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.ArgumentMatchers.isA;
+import static org.mockito.Mockito.when;
+
+import au.com.dius.pact.provider.junit5.HttpTestTarget;
+import au.com.dius.pact.provider.junit5.PactVerificationContext;
+import au.com.dius.pact.provider.junit5.PactVerificationInvocationContextProvider;
+import au.com.dius.pact.provider.junitsupport.State;
+import au.com.dius.pact.provider.junitsupport.loader.PactBroker;
+import bio.terra.common.iam.BearerToken;
+import bio.terra.common.iam.SamUser;
+import bio.terra.common.iam.SamUserFactory;
+import bio.terra.externalcreds.ExternalCredsWebApplication;
+import bio.terra.externalcreds.dataAccess.AccessTokenCacheDAO;
+import bio.terra.externalcreds.dataAccess.LinkedAccountDAO;
+import bio.terra.externalcreds.dataAccess.StatusDAO;
+import bio.terra.externalcreds.generated.model.Provider;
+import bio.terra.externalcreds.models.AccessTokenCacheEntry;
+import bio.terra.externalcreds.models.ImmutableLinkedAccount;
+import bio.terra.externalcreds.models.LinkedAccount;
+import bio.terra.externalcreds.services.OAuth2Service;
+import jakarta.servlet.http.HttpServletRequest;
+import java.sql.Timestamp;
+import java.time.Instant;
+import java.util.Map;
+import java.util.Optional;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.TestTemplate;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.core.OAuth2AccessToken;
+import org.springframework.security.oauth2.core.OAuth2AccessToken.TokenType;
+import org.springframework.security.oauth2.core.OAuth2RefreshToken;
+import org.springframework.security.oauth2.core.endpoint.OAuth2AccessTokenResponse;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(
+    webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT,
+    properties = {"server.port=8080"},
+    classes = ExternalCredsWebApplication.class)
+@ActiveProfiles({"test", "human-readable-logging"})
+@au.com.dius.pact.provider.junitsupport.Provider("ecm")
+@PactBroker
+public class VerifyServicePacts {
+  @MockBean StatusDAO statusDAO;
+  @MockBean LinkedAccountDAO linkedAccountDAO;
+  @MockBean AccessTokenCacheDAO accessTokenCacheDAO;
+  @MockBean SamUserFactory samUserFactory;
+  @MockBean OAuth2Service oAuth2Service;
+  @Mock private OAuth2AccessTokenResponse mockAccessTokenResponse;
+
+  @BeforeEach
+  void setupTestTarget(PactVerificationContext context) {
+    context.setTarget(new HttpTestTarget("localhost", 8080, "/"));
+  }
+
+  @TestTemplate
+  @ExtendWith(PactVerificationInvocationContextProvider.class)
+  void pactVerificationTestTemplate(PactVerificationContext context) {
+    context.verifyInteraction();
+  }
+
+  @State({ProviderStates.ECM_IS_OK})
+  public void ecmIsOk() {
+    when(statusDAO.isPostgresOk()).thenReturn(true);
+  }
+
+  @State({ProviderStates.USER_IS_REGISTERED})
+  public Map<String, String> userIsRegistered() {
+    String testUserEmail = "test_user@test.com";
+    String testSubjectId = "testSubjectId";
+    String testBearerToken = "testBearerToken";
+
+    OAuth2RefreshToken testRefreshToken =
+        new OAuth2RefreshToken("dummy_refresh_token", Instant.now());
+    OAuth2AccessToken testAccessToken =
+        new OAuth2AccessToken(
+            TokenType.BEARER, testBearerToken, Instant.now(), Instant.now().plusSeconds(360));
+
+    when(samUserFactory.from(isA(HttpServletRequest.class), anyString()))
+        .thenReturn(new SamUser(testUserEmail, testSubjectId, new BearerToken(testBearerToken)));
+
+    ImmutableLinkedAccount testAccount =
+        new LinkedAccount.Builder()
+            .id(0)
+            .userId(testUserEmail)
+            .provider(Provider.GITHUB)
+            .refreshToken(testRefreshToken.getTokenValue())
+            .expires(Timestamp.from(Instant.now().plusSeconds(360)))
+            .externalUserId("dummy external_user_id")
+            .isAuthenticated(true)
+            .build();
+
+    AccessTokenCacheEntry accessTokenCacheEntry =
+        new AccessTokenCacheEntry.Builder()
+            .linkedAccountId(0)
+            .accessToken(testAccessToken.getTokenValue())
+            .expiresAt(Instant.now())
+            .build();
+
+    when(linkedAccountDAO.getLinkedAccount(eq(testSubjectId), eq(Provider.GITHUB)))
+        .thenReturn(Optional.of(testAccount));
+
+    when(accessTokenCacheDAO.upsertAccessTokenCacheEntry(isA(AccessTokenCacheEntry.class)))
+        .thenReturn(accessTokenCacheEntry);
+
+    when(mockAccessTokenResponse.getRefreshToken()).thenReturn(testRefreshToken);
+    when(mockAccessTokenResponse.getAccessToken()).thenReturn(testAccessToken);
+
+    when(oAuth2Service.authorizeWithRefreshToken(isA(ClientRegistration.class), any(), any()))
+        .thenReturn(mockAccessTokenResponse);
+
+    // These values are returned so that they can be injected into variables in the Pact(s)
+    return Map.of(
+        "userEmail", testUserEmail,
+        "bearerToken", testBearerToken);
+  }
+}
