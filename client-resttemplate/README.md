2fc703d2ed9c2a96280f0553603c0bdedf5c6db4
Michael Spector <michael.l.spector@gmail.com>
2024-04-18 11:48:33 -0400

    update README and comment

--- client-resttemplate/README.md
+++ client-resttemplate/README.md
@@ -7,3 +7,11 @@ To publish a new version of this client library:
 1. Optionally, update `subprojects.version` in [the top-level build.gradle](../build.gradle)
 2. `cd client-resttemplate`
 3. run `./publish.sh`
+
+## Build a local JAR for development
+This can be useful when developing contract tests (e.g. when adding new Provider States)
+
+1. Navigate to the `client-resttemplate` directory (where this README is located)
+2. Run `../gradlew assemble`
+3. Find the generated JAR file in `./build/libs`
+4. Replace the dependency in your other project with the newly generated JAR
--- client-resttemplate/src/main/java/bio/terra/externalcreds/pact/ProviderStates.java
+++ client-resttemplate/src/main/java/bio/terra/externalcreds/pact/ProviderStates.java
@@ -1,3 +1,18 @@
+/*
+The purpose of this "enum" is to provide a stable dictionary of "provider states"
+for contract testing purposes.
+("Provider" in this context shouldn't be confused with the ECM concept of an "external provider";
+here it refers to the "consumer-provider pair" in the Pact contract testing framework.)
+
+This "enum" is implemented as a normal class, so that its values can be referenced
+in Java annotations which are often used in contract testing code.
+(Most values passed into annotations must be constant at compile time.)
+
+These provider states are packaged and released as part of the ECM client module.
+You may find it useful to build a local JAR of `client-resttemplate` during development.
+See instructions on how to do that in the README at `client-resttemplate/README.md`
+ */
+
 package bio.terra.externalcreds.pact;
 
 public class ProviderStates {
