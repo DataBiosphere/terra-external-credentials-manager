295e28d03d6662e2d535dfad2e40117a7884997f
David An <davidan@broadinstitute.org>
2025-03-06 09:57:12 -0500

    minor cleanup

--- service/src/main/java/bio/terra/externalcreds/controllers/FenceAccountKeyController.java
+++ service/src/main/java/bio/terra/externalcreds/controllers/FenceAccountKeyController.java
@@ -1,23 +1,13 @@
 package bio.terra.externalcreds.controllers;
 
 import bio.terra.common.exception.NotFoundException;
-import bio.terra.externalcreds.auditLogging.AuditLogger;
 import bio.terra.externalcreds.generated.api.FenceAccountKeyApi;
 import bio.terra.externalcreds.generated.model.Provider;
-import bio.terra.externalcreds.services.FenceProviderService;
-import bio.terra.externalcreds.services.LinkedAccountService;
-import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 
 @Controller
-public record FenceAccountKeyController(
-    AuditLogger auditLogger,
-    ExternalCredsSamUserFactory samUserFactory,
-    LinkedAccountService linkedAccountService,
-    FenceProviderService fenceProviderService,
-    HttpServletRequest request)
-    implements FenceAccountKeyApi {
+public record FenceAccountKeyController() implements FenceAccountKeyApi {
 
   @Override
   public ResponseEntity<String> getFenceAccountKey(Provider provider) {
